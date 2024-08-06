654eb24b0f4d089c41726abddf16f14aaf2bc15c
Luke McCabe <luke@lukemccabe.co.uk>
2024-08-06 14:21:31 +0100

    LM/007 - Enhancing with Aceternity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
 import Layout from '../components/layout';
+import { PlaceholdersAndVanishInput } from '../components/ui/placeholders-and-vanish-input';
 
 const HomePage = () => {
   const [link, setLink] = useState('');
@@ -20,23 +21,18 @@ const HomePage = () => {
         <title>Home Page</title>
         <meta name="description" content="This is the homepage of the site." />
       </Head>
-      <div className="pageContainer">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a simple Next.js application.</p>
-      <form onSubmit={handleSubmit} className={"inputform"}>
-          <label htmlFor="linkInput">Enter a Link:</label>
-          <input
-            type="url"
+      <div className="container mx-auto p-4">
+      <PlaceholdersAndVanishInput
+        type="url"
             id="linkInput"
             name="linkInput"
             value={link}
             onChange={handleInputChange}
-            placeholder="https://www.youtube.com/watch?v=r6tH55syq0o"
+            placeholders={['https://www.youtube.com/watch?v=r6tH55syq0o']}
             required
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+        onSubmit={handleSubmit}
+      />
+    </div>
     </Layout>
   );
 };
