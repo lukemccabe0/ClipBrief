09fd7af475813c2bdf214aeada85e16d14fa9191
Luke McCabe <luke@lukemccabe.co.uk>
2024-08-07 09:58:17 +0100

    Adding Watch Parameter to API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // src/utils/api.js
 
 export const validateYouTubeUrl = (url) => {
-    const regex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+  const regex = /^(https?:\/\/)?(www\.)?(youtube\.com\/(watch\?v=)|youtu\.be\/).+$/;
     return regex.test(url);
   };
   
